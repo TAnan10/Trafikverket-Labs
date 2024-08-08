9fbe5b35a3f6a5efb9f998c305fdea3f50313f82
erkan48101 <erkan4810@gmail.com>
2024-08-08 14:20:04 +0200

    RequestsExamples in python

--- ExampleCode/C#/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ ExampleCode/C#/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf28ea9e2264e504b524d768228e048360ff058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e70628c7d7ece8e1d6a44a5ae9a61b2adc56ae77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
